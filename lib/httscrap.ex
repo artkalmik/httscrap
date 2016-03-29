d5bb88b838bfb1d8f0e0849e33c9afd6889642d2
Artyom <a.kalmykov@tarifer.ru>
2016-03-29 20:28:29 +0300

    Logging in to the Site

--- lib/httscrap.ex
+++ lib/httscrap.ex
@@ -1,2 +1,13 @@
 defmodule Httscrap do
+  use Hound.Helpers
+
+  def start do
+    IO.puts "Starting"
+    Hound.start_session
+    navigate_to "https://example.com/login"
+    find_element(:id, "user_login") |> fill_field('mylogin')
+    el = find_element(:id, "user_pass")
+    fill_field(el, 'mypass')
+    submit_element(el)
+  end
 end
